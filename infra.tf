282b3a23fa156577431813bfb9d6828b4b4f00d8
Alexis Smirnov <alexis@smirnov.ca>
2015-04-25 10:47:24 -0400

    ignore terraform's crash.log

--- infra.tf
+++ infra.tf
@@ -9,9 +9,10 @@ resource "aws_key_pair" "key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWrX3Pf/V0V2gWel4QeOrX2/o5QWSyutcB12HZ3Us7E6+mQYHo1W85KpPJunpJRkD1v1Me9YBKZjoIxFrAWLrmVJftD9vspxPibMDIx9/uu0FOhr4ttcHW4VHoj8MWmp4BGwCuRlr/bXVYUhtaFOB07L0WMLbaqC2p1DyC0buB/mFRwPvXZdkmK8tnsHhZmcGUffjuID+X/5DBpFNvI72Bute7c9EahtKpomFjXXu93EwqhHFK9pGCsNtYSmWn4Oiyj6Rs5VIS0PH0ef73tFb1yl9Cnz1jO17OxffC1zYNZZbDbyF78PS5afeSz0v+BOipGbkLM5JbeDX/jgT0eEXR alexis@icecream.local"
 }
 
-resource "aws_eip" "ip" {
-    instance = "${aws_instance.spark.id}"
-}
+#resource "aws_kinesis_stream" "spark_in" {
+#    stream_name = "spark_in"
+#    shard_count = "2"
+#}
 
 resource "aws_instance" "spark" {
     ami = "ami-1ecae776" # AWS Linux AMI
